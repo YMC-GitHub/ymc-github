2a7264f377e82d960adccc6e679784878382b6d3
yemiancheng <ymc.github@gmail.com>
2022-10-16 20:06:11 +0800

    chore(core): record updated time in file

--- bin/render-readme.js
+++ bin/render-readme.js
@@ -21,7 +21,7 @@ async function main() {
     // log(encodeAsciiToUri(data))
     // log(encoded);
     // encoded = encodeURIComponent(`https://awesome-profile.vercel.app/api/profile?wakatime=ymc_github&github={owner}&name=YeMiancheng&job=Fullstack%20developer&theme=dark&aboutMe=${data}`);
-    log(encoded)
+    log(encoded);
     // encoded = "https://awesome-profile.vercel.app/api/profile?wakatime=ymc_github&github={owner}&name=YeMiancheng&job=Fullstack%20developer&theme=dark&aboutMe=%49%27%6d%20%61%20%66%75%6c%6c%73%74%61%63%6b%20%64%65%76%65%6c%6f%70%65%72%2c%20%49%27%76%65%20%62%65%65%6e%20%63%6f%64%69%6e%67%20%73%69%6e%63%65%20%49%20%77%61%73%20%31%38%20%79%65%61%72%73%20%6f%6c%64%2e%20%49%6e%20%32%30%31%32%20%49%20%65%6e%74%65%72%65%64%20%74%68%65%20%4e%61%6e%6a%69%6e%67%20%41%67%72%69%63%75%6c%74%75%72%61%6c%20%55%6e%69%76%65%72%73%69%74%79%20%77%68%65%72%65%20%49%20%73%74%61%72%74%65%64%20%73%74%75%64%79%69%6e%67%20%4e%61%74%75%72%65%20%53%63%69%65%6e%63%65%20%61%6e%64%20%43%6f%6d%70%75%74%65%72%20%53%63%69%65%6e%63%65%2c%20%77%68%65%72%65%20%49%20%6d%65%74%20%69%6e%63%72%65%64%69%62%6c%65%20%70%65%6f%70%6c%65%2e%20%49%20%61%6d%20%63%75%72%72%65%6e%74%6c%79%20%77%6f%72%6b%69%6e%67%20%6f%6e%20%46%6c%6f%77%65%72%54%65%61%72%2c%20%61%20%73%6f%66%74%77%61%72%65%20%66%61%63%74%6f%72%79%20%77%68%65%72%65%20%49%20%77%6f%72%6b%20%6d%61%69%6e%6c%79%20%77%69%74%68%20%52%65%61%63%74%2c%20%56%75%65%6a%73%20%61%6e%64%20%4e%6f%64%65%4a%53%2e"
     // log(encoded)
 
@@ -33,17 +33,30 @@ async function main() {
         name: "data/readme.tpl.md",
         data: "",
     };
-    
-    log(`[info] tpl: ${file.name}`)
-    log(`[info] load tpl`)
+
+    log(`[info] tpl: ${file.name}`);
+    log(`[info] load tpl`);
     file.data = readTextSync(file.name, "");
 
-    log(`[info] render tpl`)
-    file.data=writeTpl(file.data,{owner:'ymc-github',aboutme:encoded,authorName:'YeMiancheng'})
+    log(`[info] render tpl`);
+    file.data = writeTpl(file.data, {
+        owner: "ymc-github",
+        aboutme: encoded,
+        authorName: "YeMiancheng",
+    });
 
-    file.name=`README.md`
-    log(`[info] out: ${file.name}`)
-    writeTextSync(file.name,file.data)
+    file.name = `README.md`;
+    log(`[info] out: ${file.name}`);
+    writeTextSync(file.name, file.data);
+
+    //feat: record updated time in file
+    let now = new Date();
+    now = formtDate("yyyy-MM-dd HH:mm:ss",now);
+    log(`[info] now:${now}`)
+    file.data = now;
+    file.name = `updated-time.md`;
+    log(`[info] out: ${file.name}`);
+    writeTextSync(file.name, file.data);
 }
 function encodeAsciiToUri(list = "") {
     let res = "";
@@ -100,6 +113,82 @@ function writeTpl(tpl, data) {
     return (v) => renderTpl(tpl, v);
 }
 
+/**
+ * format date
+ * @param {string} fmt
+ * @returns {string}
+ * @sample
+ * ```
+ * let now = new Date();
+ * formtDate("yyyy-MM-dd HH:mm:ss",now);
+ * ```
+ * @description
+ * ```
+ * M+
+ * ```
+ */
+function formtDate(fmt,ctx) {
+    // let ctx = this;
+    const o = {
+        "M+": ctx.getMonth() + 1,
+        "d+": ctx.getDate(),
+        "H+": ctx.getHours(),
+        "m+": ctx.getMinutes(),
+        "s+": ctx.getSeconds(),
+        "S+": ctx.getMilliseconds(),
+    };
+    // log(fmt, o);
+    // get year eg . formtDate('yyyy')//2022
+    let reg;
+    reg = /(y+)/;
+    if (reg.test(fmt)) {
+        // deprecated
+        // fmt = fmt.replace(
+        //   RegExp.$1,
+        //   `${ctx.getFullYear()}`.substr(4 - RegExp.$1.length)
+        // );
+        fmt = fmt.replace(reg, (x) =>
+            `${ctx.getFullYear()}`.substring(4 - x.length)
+        );
+    }
+    // add 0 before result with length
+    for (const k in o) {
+        reg = new RegExp(`(${k})`);
+        if (reg.test(fmt)) {
+            // deprecated
+            // fmt = fmt.replace(
+            //   RegExp.$1,
+            //   RegExp.$1.length == 1 ? o[k] : `00${o[k]}`.substr(String(o[k]).length)
+            // );
+            fmt = fmt.replace(reg, (x) =>
+                x.length == 1
+                    ? o[k]
+                    : `00${o[k]}`.substring(String(o[k]).length)
+            );
+        }
+    }
+    return fmt;
+}
+// extend date 
+// formtDate
+
+/**
+ * format date
+ * @param {string} fmt
+ * @returns {string}
+ * @sample
+ * ```
+ * let now = new Date();
+ * now.Format("yyyy-MM-dd HH:mm:ss");
+ * ```
+ * @description
+ * ```
+ * M+
+ * ```
+ */
+Date.prototype.Format = function(fmt){
+    return formtDate(fmt,this)
+}
 main();
 
 // node bin/render-readme.js
